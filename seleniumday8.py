f3eb99d98c4a039df012e24385d4b22ca9856274
33TChaubey <chaubeytn2103@outlook.com>
2026-01-11 23:25:33 +0530

    windows handling tried

--- seleniumday8.py
+++ seleniumday8.py
@@ -3,9 +3,23 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver import ActionChains
+
+
 
 service_obj = Service("C://Users//chaub//OneDrive//Desktop//chromedriver.exe")
 
 driver = webdriver.Chrome(service=service_obj)
-driver.get("https://testautomationpractice.blogspot.com/")
+driver.get("https://demo.automationtesting.in/Register.html")
 driver.maximize_window()
+driver.implicitly_wait(5)
+action=ActionChains(driver)
+interactions=driver.find_element(By.XPATH, "//a[contains(text(), 'Interactions ')]")
+draganddrop=driver.find_element(By.XPATH, "//a[contains(text(), 'Drag and Drop ')]")
+static=driver.find_element(By.XPATH, "//a[contains(text(),'Static ')]")
+action.move_to_element(interactions).perform()
+time.sleep(5)
+action.move_to_element(draganddrop).perform()
+time.sleep(5)
+static.click()
+time.sleep(5)
